df1b38074a527c460319c35c626151f1108db489
Hariharan <104617937+Hariharan-G96@users.noreply.github.com>
2022-12-28 22:35:18 +0530

    getAllTickets and getTicketById added

---
+++ 10. Building the CRM App - Ticket Manipulations and Admin Capabilities Continued(28-12-22)/models/user.model.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+    },
+    userId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required : true,
+        lowerCase : true,
+        unique : true,
+        minLength : 10,
+        isEmail : true
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 8
+    },
+    createdAt : {
+        type : Date,
+        immutable : true,
+        default : () => Date.now()
+    },
+    updatedAt : {
+        type : Date,
+        default : () => Date.now()
+    },
+    userType : {
+        type : String,
+        required : true,
+        default : "CUSTOMER"
+    },
+    userStatus : {
+        type : String,
+        required : true,
+        default : "APPROVED"
+    },
+    ticketsCreated : {
+        type : [mongoose.SchemaTypes.ObjectId],
+        ref : "Ticket" // Refers the Ticket model
+    },
+    ticketsAssigned : {
+        type : [mongoose.SchemaTypes.ObjectId],
+        ref : "Ticket" // Refers the Ticket model
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
